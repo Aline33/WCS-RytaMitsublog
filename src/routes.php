a79e9be6885e899c17b819c31233542efe73b7ef
Aline LEGENDRE <legendrealine@gmail.com>
2023-04-24 17:12:21 +0200

    added the route for the add article page

--- src/routes.php
+++ src/routes.php
@@ -14,4 +14,5 @@ return [
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
     'article/show' => ['ArticleController', 'show',],
+    'article/add' => ['ArticleController', 'add'],
 ];
