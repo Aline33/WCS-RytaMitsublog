cfc69817ac3007fb24d9c45d05e7086d69fb07a1
Urzaag <remi.meslin.gm@gmail.com>
2023-04-25 16:17:38 +0200

    code indented

--- src/Model/AbstractManager.php
+++ src/Model/AbstractManager.php
@@ -40,7 +40,10 @@ abstract class AbstractManager
     public function selectOneById(int $id): array|false
     {
         // prepared request
-        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE " . $this->getPrimaryKeyNameFromTable() . "=:id");
+        $statement = $this->pdo->prepare("
+            SELECT *
+            FROM " . static::TABLE . "
+            WHERE " . $this->getPrimaryKeyNameFromTable() . "=:id");
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
 
@@ -53,7 +56,10 @@ abstract class AbstractManager
     public function delete(int $id): void
     {
         // prepared request
-        $statement = $this->pdo->prepare("DELETE FROM " . static::TABLE . " WHERE " . $this->getPrimaryKeyNameFromTable() . "=:id");
+        $statement = $this->pdo->prepare("
+            DELETE
+            FROM " . static::TABLE . "
+            WHERE " . $this->getPrimaryKeyNameFromTable() . "=:id");
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
     }
