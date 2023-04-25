afe8003c41cae21715a6507abffa582fde6a7ea7
Urzaag <remi.meslin.gm@gmail.com>
2023-04-25 13:26:42 +0200

    main pictures are now distinguished

--- src/Model/ArticleManager.php
+++ src/Model/ArticleManager.php
@@ -10,7 +10,7 @@ class ArticleManager extends AbstractManager
 
     public function getPictures(int $id): array|false
     {
-        $statement = $this->pdo->prepare("SELECT link FROM bt_picture WHERE " . $this->getForeignKeyName(static::TABLE) . " = :id");//"SELECT link FROM bt_picture WHERE " . $this->getForeignKeyName(static::TABLE) . " = :id"
+        $statement = $this->pdo->prepare("SELECT link FROM bt_picture WHERE " . $this->getForeignKeyName(static::TABLE) . " = :id ORDER BY is_main DESC");//"SELECT link FROM bt_picture WHERE " . $this->getForeignKeyName(static::TABLE) . " = :id"
         $statement->bindValue(':id', $id, PDO::PARAM_INT);
         $statement->execute();
 
