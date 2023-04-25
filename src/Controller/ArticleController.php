5a9aaec433e784feabea6b49aa6ff991181d303e
Urzaag <remi.meslin.gm@gmail.com>
2023-04-25 13:30:00 +0200

    pictures are organised: main picture first

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,13 +11,30 @@ class ArticleController extends AbstractController
         $articleManager = new ArticleManager();
         $article = $articleManager->selectOneById($id);
         $bodyArticleSplit = $this->splitArticleText($article['body_article']);
-        $picture = $articleManager->getPictures($id);
+        $pictures = $articleManager->getPictures($id);
+        $pictures = $this->organisePictures($pictures);
 
-        return $this->twig->render('Article/show.html.twig', ['article' => $article, 'bodyArticleSplit' => $bodyArticleSplit, 'picture' => $picture]);
+        return $this->twig->render('Article/show.html.twig', ['article' => $article, 'bodyArticleSplit' => $bodyArticleSplit, 'picture' => $pictures]);
     }
 
     public function splitArticleText(string $bodyArticle): array
     {
         return str_split($bodyArticle, strlen($bodyArticle) / 3);
+    }
+
+    public function organisePictures(array $pictures): array
+    {
+        $i = 1;
+        foreach ($pictures as $key => $picture) {
+            if ($key === 0) {
+                $pictures['pictureMain'] = $pictures[$key];
+                unset($pictures[$key]);
+            } else {
+                $pictures['picture' . $i] = $pictures[$key];
+                unset($pictures[$key]);
+            }
+            $i++;
+        }
+        return $pictures;
     }
 }
