f1abd15e1d79854327a83f4c0a63159736fed062
Urzaag <remi.meslin.gm@gmail.com>
2023-04-26 15:33:04 +0200

    method to sanitize data WIP

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -12,6 +12,33 @@ use Twig\Loader\FilesystemLoader;
  */
 abstract class AbstractController
 {
+    public const FILTERS = [
+        'string' => FILTER_UNSAFE_RAW,
+        'string[]' => [
+            'filter' => FILTER_UNSAFE_RAW,
+            'flags' => FILTER_REQUIRE_ARRAY
+        ],
+        'email' => FILTER_SANITIZE_EMAIL,
+        'int' => [
+            'filter' => FILTER_SANITIZE_NUMBER_INT,
+            'flags' => FILTER_REQUIRE_SCALAR
+        ],
+        'int[]' => [
+            'filter' => FILTER_SANITIZE_NUMBER_INT,
+            'flags' => FILTER_REQUIRE_ARRAY
+        ],
+        'float' => [
+            'filter' => FILTER_SANITIZE_NUMBER_FLOAT,
+            'flags' => FILTER_FLAG_ALLOW_FRACTION
+        ],
+        'float[]' => [
+            'filter' => FILTER_SANITIZE_NUMBER_FLOAT,
+            'flags' => FILTER_REQUIRE_ARRAY
+        ],
+        'url' => FILTER_SANITIZE_URL
+    ];
+    public const FIELDS = [];
+    public const OPTIONS = [];
     protected Environment $twig;
     protected array | false $user;
 
@@ -31,4 +58,10 @@ abstract class AbstractController
         $this->user = isset($_SESSION['user_id']) ? $userManager->selectOneById($_SESSION['user_id']) : false;
         $this->twig->addGlobal('user', $this->user);
     }
+
+    /*public function sanitizeData(array $inputs, array $fields, array $filters = self::FILTERS): array
+    {
+        $options = array_map(fn($field) => static::FILTERS[$field], $fields);
+        return filter_var_array($inputs, $options);
+    }*/
 }
