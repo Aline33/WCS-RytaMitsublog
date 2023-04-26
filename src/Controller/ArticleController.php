8d8df8e9badcbf34d1841c9a8d6c0a75e45ba65d
Aline LEGENDRE <legendrealine@gmail.com>
2023-04-26 12:06:23 +0200

    fix Grum line too long and added add function

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -12,9 +12,13 @@ class ArticleController extends AbstractController
         $article = $articleManager->selectOneById($id);
         $bodyArticleSplit = $this->splitArticleText($article['body_article']);
         $pictures = $articleManager->getPictures($id);
-        $pictures = $this->organisePictures($pictures);
+       /* $pictures = $this->organisePictures($pictures);*/
 
-        return $this->twig->render('Article/show.html.twig', ['article' => $article, 'bodyArticleSplit' => $bodyArticleSplit, 'picture' => $pictures]);
+        return $this->twig->render('Article/show.html.twig', [
+            'article' => $article,
+            'bodyArticleSplit' => $bodyArticleSplit,
+            'picture' => $pictures
+        ]);
     }
 
     public function splitArticleText(string $bodyArticle): array
@@ -22,7 +26,7 @@ class ArticleController extends AbstractController
         return str_split($bodyArticle, strlen($bodyArticle) / 3);
     }
 
-    public function organisePictures(array $pictures): array
+    /*public function organisePictures(array $pictures): array
     {
         $iterator = 1;
         foreach ($pictures as $key => $picture) {
@@ -36,10 +40,24 @@ class ArticleController extends AbstractController
             $iterator++;
         }
         return $pictures;
-    }
+    }*/
 
     public function add(): string
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $article = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $articleManager = new ArticleManager();
+            $id = $articleManager->insert($article);
+
+            $this->show($id);
+            //header('Location:/article/show?id=' . $id);
+        }
         return $this->twig->render('Article/add.html.twig');
     }
+    //TODO: function addPicture
 }
