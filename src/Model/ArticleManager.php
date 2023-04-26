6a0d654fa14686d83831e4bc7f753fdc4665386f
Aline LEGENDRE <legendrealine@gmail.com>
2023-04-26 12:10:51 +0200

    fix Grum line too long and added insert function

--- src/Model/ArticleManager.php
+++ src/Model/ArticleManager.php
@@ -10,10 +10,29 @@ class ArticleManager extends AbstractManager
 
     public function getPictures(int $id): array|false
     {
-        $statement = $this->pdo->prepare("SELECT link FROM bt_picture WHERE " . $this->getForeignKeyName(static::TABLE) . " = :id ORDER BY is_main DESC");//"SELECT link FROM bt_picture WHERE " . $this->getForeignKeyName(static::TABLE) . " = :id"
+        //"SELECT link FROM bt_picture WHERE " .
+        // $this->getForeignKeyName(static::TABLE) . " = :id"
+        $query = "SELECT link FROM bt_picture WHERE " .
+            $this->getForeignKeyName(static::TABLE) .
+            " = :id ORDER BY is_main DESC";
+        $statement = $this->pdo->prepare($query);
         $statement->bindValue(':id', $id, PDO::PARAM_INT);
         $statement->execute();
 
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function insert(array $article) : int
+    {
+        $query = "INSERT INTO " . self::TABLE . "(user_id, title, description_article, body_article) VALUES (:user_id, :title, :description, :body)";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('user_id', 1, PDO::PARAM_INT); // TODO: change user_id value
+        $statement->bindValue(':title', $article['title'], PDO::PARAM_STR);
+        $statement->bindValue(':description', $article['description'], PDO::PARAM_STR);
+        $statement->bindValue(':body', $article['content'], PDO::PARAM_STR);
+        $statement->execute();
+
+        return (int)$this->pdo->lastInsertId();
+    }
+
 }
