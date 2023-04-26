5b958a292c0ff0441dc90dbb97ccc5095980e637
Urzaag <remi.meslin.gm@gmail.com>
2023-04-26 17:20:20 +0200

    method to sanitize data WIP

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,30 +6,39 @@ use App\Model\UserManager;
 
 class UserController extends AbstractController
 {
+    public const FIELDS = [
+        'first_name' => 'string',
+        'last_name' => 'string',
+        'username' => 'string',
+        'email' => 'email',
+        'birthday' => 'date',
+        'password' => 'string'
+    ];
+
     // TODO : get data from $_POST,
     // TODO :  verify their safety,
     // TODO : select one user corresponding to email or username,
     // TODO : check compatibility
-    // TODO :  set global $user used to test when the user is connceted or not
-    public function login(): string
+    // TODO :  set global $user used to test when the user is connected or not
+    public function login(): void
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $userLogin = array_map('trim', $_POST);
-            $userLogin = array_map('htmlentities', $userLogin);
+            $userLogin = $this->sanitizeData($_POST, self::FIELDS);
 
             $password = $userLogin['password'];
 
             $userManager = new UserManager();
-            $user = $userManager->selectOneByEmail($userLogin['email']);
+            $user = $userManager->selectOneByUsername($userLogin['username']);
 
-            $email = $user['email'];
+            $username = $user['user_name'];
             $hashPassword = $user['user_password'];
 
-            if (password_verify($password, $hashPassword)) {
+            if ($password === $hashPassword) { // password_verify($password, $hashPassword)
                 $_SESSION['user_id'] = $user['id_user'];
-                $_SESSION['user_email'] = $email;
+                $_SESSION['username'] = $username;
+                header('Location: /article/show?id=1');
             }
         }
-        return $this->twig->render('');
+        //return $this->twig->render('');
     }
 }
