b63e6024144b730989a1df0aeb1fd1bae206fa91
Urzaag <remi.meslin.gm@gmail.com>
2023-04-26 13:36:33 +0200

    methods to select one User added

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -2,7 +2,29 @@
 
 namespace App\Model;
 
+use PDO;
+
 class UserManager extends AbstractManager
 {
+    public const TABLE = 'bt_user';
     // TODO : create methods to get user info depending on one field, (email and username would be good)
+    public function selectOneByEmail(string $email): array
+    {
+        $query = "SELECT * FROM " . static::TABLE . " WHERE email = :email";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':email', $email, PDO::PARAM_STR);
+        $statement->execute();
+
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function selectOneByUsername(string $username): array
+    {
+        $query = "SELECT * FROM " . static::TABLE . " WHERE user_name = :username";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':user_name', $username, PDO::PARAM_STR);
+        $statement->execute();
+
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
 }
