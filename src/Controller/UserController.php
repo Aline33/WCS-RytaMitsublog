28eb414ed3bf62fccab9debdae8b0e4615f2ffb8
Urzaag <remi.meslin.gm@gmail.com>
2023-04-26 13:35:10 +0200

    method to login added

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Model\UserManager;
+
 class UserController extends AbstractController
 {
     // TODO : get data from $_POST,
@@ -9,7 +11,25 @@ class UserController extends AbstractController
     // TODO : select one user corresponding to email or username,
     // TODO : check compatibility
     // TODO :  set global $user used to test when the user is connceted or not
-    public function login()//: string
+    public function login(): string
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $userLogin = array_map('trim', $_POST);
+            $userLogin = array_map('htmlentities', $userLogin);
+
+            $password = $userLogin['password'];
+
+            $userManager = new UserManager();
+            $user = $userManager->selectOneByEmail($userLogin['email']);
+
+            $email = $user['email'];
+            $hashPassword = $user['user_password'];
+
+            if (password_verify($password, $hashPassword)) {
+                $_SESSION['user_id'] = $user['id_user'];
+                $_SESSION['user_email'] = $email;
+            }
+        }
+        return $this->twig->render('');
     }
 }
