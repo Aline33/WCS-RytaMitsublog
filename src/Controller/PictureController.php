0ce040577088e928824c5b491da7088cb6337ee9
Aline LEGENDRE <legendrealine@gmail.com>
2023-04-27 12:24:57 +0200

    added recording of article images in the database

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -2,6 +2,42 @@
 
 namespace App\Controller;
 
-class PictureController
+use App\Model\PictureManager;
+
+class PictureController extends AbstractController
 {
+    public function add(int $id): void
+    {
+        $uploadDir = 'assets/images/';
+        if (isset($_FILES)) {
+            foreach ($_FILES as $picture) {
+                if ($picture['error'] == UPLOAD_ERR_NO_FILE) {
+                    continue;
+                }
+                $extension = pathinfo($picture['name'], PATHINFO_EXTENSION);
+                $authorizedExtensions = ['jpg', 'png', 'gif', 'webp'];
+                $maxFileSize = 2097152;
+
+                $file = $picture['name'] . uniqid('', false) . "." . $extension;
+                $files[] = $file;
+                $uploadFile = $uploadDir . $file;
+
+                if (!in_array($extension, $authorizedExtensions)) {
+                    $errors[] = "Veuillez sélectionner une image de type jpg, png, gif ou webp";
+                }
+                if (file_exists($picture['tmp_name']) && filesize($picture['tmp_name']) > $maxFileSize) {
+                    $errors[] = "Votre fichier doit faire moins de 2MO !";
+                }
+                if (empty($errors)) {
+                    move_uploaded_file($picture['tmp_name'], $uploadFile);
+                }
+            }
+            echo '<pre>';
+            var_dump($files);
+            echo '</pre>';
+
+            $pictureManager = new PictureManager();
+            $pictureManager->insert($files, $id);
+        }
+    }
 }
--- src/Model/PictureManager.php
+++ src/Model/PictureManager.php
@@ -2,7 +2,28 @@
 
 namespace App\Model;
 
+use PDO;
+
 class PictureManager extends AbstractManager
 {
     public const TABLE = 'bt_picture';
+
+    public function insert(array $files, int $id): void
+    {
+        foreach ($files as $position => $file) {
+            if ($position === 0) {
+                $is_main = 1;
+            } else {
+                $is_main = 0;
+            }
+            $query = "INSERT INTO " . self::TABLE .
+                "(article_id, link, is_main)
+            VALUES (:article_id, :link, :is_main)";
+            $statement = $this->pdo->prepare($query);
+            $statement->bindValue('article_id', $id,PDO::PARAM_INT);
+            $statement->bindValue(':link', $file, PDO::PARAM_STR);
+            $statement->bindValue(':is_main', $is_main,);
+            $statement->execute();
+        }
+    }
 }
