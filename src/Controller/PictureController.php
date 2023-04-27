8a87f17d5b633ad38c535aac3714fd79e04f900a
Aline LEGENDRE <legendrealine@gmail.com>
2023-04-27 12:27:34 +0200

    fix grum

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -9,35 +9,32 @@ class PictureController extends AbstractController
     public function add(int $id): void
     {
         $uploadDir = 'assets/images/';
-        if (isset($_FILES)) {
-            foreach ($_FILES as $picture) {
-                if ($picture['error'] == UPLOAD_ERR_NO_FILE) {
-                    continue;
-                }
-                $extension = pathinfo($picture['name'], PATHINFO_EXTENSION);
-                $authorizedExtensions = ['jpg', 'png', 'gif', 'webp'];
-                $maxFileSize = 2097152;
 
-                $file = $picture['name'] . uniqid('', false) . "." . $extension;
-                $files[] = $file;
-                $uploadFile = $uploadDir . $file;
-
-                if (!in_array($extension, $authorizedExtensions)) {
-                    $errors[] = "Veuillez sélectionner une image de type jpg, png, gif ou webp";
-                }
-                if (file_exists($picture['tmp_name']) && filesize($picture['tmp_name']) > $maxFileSize) {
-                    $errors[] = "Votre fichier doit faire moins de 2MO !";
-                }
-                if (empty($errors)) {
-                    move_uploaded_file($picture['tmp_name'], $uploadFile);
-                }
+        $files = [];
+        $errors = [];
+        foreach ($_FILES as $picture) {
+            if ($picture['error'] == UPLOAD_ERR_NO_FILE) {
+                continue;
             }
-            echo '<pre>';
-            var_dump($files);
-            echo '</pre>';
+            $extension = pathinfo($picture['name'], PATHINFO_EXTENSION);
+            $authorizedExtensions = ['jpg', 'png', 'gif', 'webp'];
+            $maxFileSize = 2097152;
 
-            $pictureManager = new PictureManager();
-            $pictureManager->insert($files, $id);
+            $file = $picture['name'] . uniqid('', false) . "." . $extension;
+            $files[] = $file;
+            $uploadFile = $uploadDir . $file;
+
+            if (!in_array($extension, $authorizedExtensions)) {
+                $errors[] = "Veuillez sélectionner une image de type jpg, png, gif ou webp";
+            }
+            if (file_exists($picture['tmp_name']) && filesize($picture['tmp_name']) > $maxFileSize) {
+                $errors[] = "Votre fichier doit faire moins de 2MO !";
+            }
+            if (empty($errors)) {
+                move_uploaded_file($picture['tmp_name'], $uploadFile);
+            }
         }
+        $pictureManager = new PictureManager();
+        $pictureManager->insert($files, $id);
     }
 }
--- src/Model/PictureManager.php
+++ src/Model/PictureManager.php
@@ -12,17 +12,17 @@ class PictureManager extends AbstractManager
     {
         foreach ($files as $position => $file) {
             if ($position === 0) {
-                $is_main = 1;
+                $isMain = 1;
             } else {
-                $is_main = 0;
+                $isMain = 0;
             }
             $query = "INSERT INTO " . self::TABLE .
                 "(article_id, link, is_main)
             VALUES (:article_id, :link, :is_main)";
             $statement = $this->pdo->prepare($query);
-            $statement->bindValue('article_id', $id,PDO::PARAM_INT);
+            $statement->bindValue(':article_id', $id, PDO::PARAM_INT);
             $statement->bindValue(':link', $file, PDO::PARAM_STR);
-            $statement->bindValue(':is_main', $is_main,);
+            $statement->bindValue(':is_main', $isMain, PDO::PARAM_BOOL);
             $statement->execute();
         }
     }
