19ea0cb04a980ce97e0eeb8939863f8b3f718683
Urzaag <remi.meslin.gm@gmail.com>
2023-05-04 13:19:35 +0200

    no more data validation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -92,29 +92,32 @@ class UserController extends AbstractController
         $user = $userManager->selectOneById($id);
         //$user['user_password'] =
 
+        //$errors = [];
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['editUserSubmit'])) {
-            $userEdit = array_pop($_POST);
+            array_pop($_POST);
+            $userEdit = $_POST;
             $userEdit = $this->sanitizeData($userEdit, self::FIELDS_EDIT);
-            $errors = $this->validateData($userEdit, self::FIELDS_EDIT);
+            //$errors = $this->validateData($userEdit, self::FIELDS_EDIT);
 
             $userEdit['id'] = $_SESSION['user_id'];
 
 
-            if (empty($errors)) {
+            //if (empty($errors)) {
                 $userManager = new UserManager();
                 $id = $userManager->update($userEdit);
                 header('Location: /profil/edit');
                 //$_SESSION['user_id'] = $id;
                 //$_SESSION['username'] = $userEdit['new-username'];
-            }
+            //}
         }
 
-        if (!empty($errors)) {
+        /*if (!empty($errors)) {
             return $this->twig->render('Profile/edit.html.twig', [
                 'user' => $user,
                 'errors' => $errors
             ]);
-        }
+        }*/
         return $this->twig->render('Profile/edit.html.twig', [
             'user' => $user,
         ]);
