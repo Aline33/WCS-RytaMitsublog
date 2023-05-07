fa8b660b23c212c280af66f62eceaec403cd2f4d
Urzaag <remi.meslin.gm@gmail.com>
2023-05-07 15:45:02 +0200

    navbarController added

---
+++ src/Controller/NavbarController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Couchbase\User;
+use PHP_CodeSniffer\Standards\PEAR\Sniffs\Files\IncludingFileSniff;
+
+class NavbarController extends AbstractController
+{
+    /*public function login(): array
+    {
+        $userController = new UserController();
+        return $errors = $userController->login();
+    }
+
+    public function register(): array
+    {
+        $userController = new UserController();
+        return $errors = $userController->register();
+    }
+
+    public function disconnect(): void
+    {
+        $userController = new UserController();
+    }*/
+}
