e6502814e6a1a766ce1f44bf5f90cf80d9f6350f
Urzaag <remi.meslin.gm@gmail.com>
2023-05-07 19:00:43 +0200

    method to login/register is possible on every page

--- src/Controller/NavbarController.php
+++ src/Controller/NavbarController.php
@@ -2,25 +2,54 @@
 
 namespace App\Controller;
 
-use Couchbase\User;
+use App\Model\UserManager;
 use PHP_CodeSniffer\Standards\PEAR\Sniffs\Files\IncludingFileSniff;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
 
 class NavbarController extends AbstractController
 {
-    /*public function login(): array
+    public function login(): void
     {
-        $userController = new UserController();
-        return $errors = $userController->login();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['loginSubmit'])) {
+            $userController = new UserController();
+            $_SESSION['errorsLogin'] = $userController->login();
+            header('Location: http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        }
     }
 
-    public function register(): array
+    public function register(): void
     {
-        $userController = new UserController();
-        return $errors = $userController->register();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $userController = new UserController();
+            $_SESSION['errorsRegister'] = $userController->register();
+            header('Location: http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        }
     }
 
     public function disconnect(): void
     {
         $userController = new UserController();
-    }*/
+        $userController->disconnect();
+        header('Location: http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    }
+
+    public function modalLogin(): void
+    {
+        if (isset($_POST['loginSubmit']) || isset($_POST['registerSubmit']) || isset($_POST['disconnectSubmit'])) {
+            if (isset($_POST['loginSubmit'])) {
+                $this->login();
+            }
+            if (isset($_POST['registerSubmit'])) {
+                $this->register();
+            }
+            if (isset($_POST['disconnectSubmit'])) {
+                $this->disconnect();
+            }
+        } else {
+            unset($_SESSION['errorsLogin']);
+            unset($_SESSION['errorsRegister']);
+        }
+    }
 }
