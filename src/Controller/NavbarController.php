9acf96d30724d0d5b5bb7efca6de897521a3bae2
Urzaag <remi.meslin.gm@gmail.com>
2023-05-09 22:01:06 +0200

    method to search added

--- src/Controller/NavbarController.php
+++ src/Controller/NavbarController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Model\ArticleManager;
+
 class NavbarController extends AbstractController
 {
     public function login(): void
@@ -45,5 +47,31 @@ class NavbarController extends AbstractController
             unset($_SESSION['errorsLogin']);
             unset($_SESSION['errorsRegister']);
         }
+    }
+
+    public function search(): ?array
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['searchSubmit']) && !empty($_GET['querySearch'])) {
+            array_pop($_GET);
+            $querySearch = trim($_GET['querySearch']);
+            $querySearch = htmlspecialchars($querySearch);
+            $querySearch = htmlentities($querySearch);
+
+            //$articleController = new ArticleController();
+            //$articleController->search($querySearch);
+            $articleManager = new ArticleManager();
+            $pictureController = new PictureController();
+            $results = $articleManager->searchTitle($querySearch);
+            $finalResults = [];
+            foreach ($results as $result) {
+                $id = $articleManager->selectOneByTitle($result['title'])['id_article'];
+                $result['pictures'] = $articleManager->getPictures($id);
+                $result['pictures'] = $pictureController->organisePictures($result['pictures']);
+                $result['author'] = $articleManager->getAuthor($id);
+                $finalResults[] = $result;
+            }
+            return $finalResults;
+        }
+        return null;
     }
 }
